8f41cdffe73da9982a3c902b73bc14285d77fe7d
Kirple <kurt@example.com>
2018-06-05 15:57:39 -0400

    Finish working and refactored palindrome method

--- index.js
+++ index.js
@@ -18,13 +18,7 @@ function Phrase(content) {
   // For example:
   //   new Phrase("Hello, world!").letters() === "Helloworld"
   this.letters = function letters() {
-    let theLetters = [];
-    for (let i = 0; i < this.content.length; i++) {
-      if (this.content.charAt(i).match(/[a-zA-Z]/)) {
-        theLetters.push(this.content.charAt(i));
-      }
-    }
-    return theLetters.join("");
+    return Array.from(this.content).filter(c => c.match(/[a-z]/i)).join("");
   }
 
   // Returns true if the phrase is a palindrome, false otherwise.
